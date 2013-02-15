4bae51c674e076e1c5a4872cf7706ae5ce467f9c
Jennifer Ecker <drjecker@gmail.com>
2013-02-15 13:37:55 -0500

    Setting up

--- page.php
+++ page.php
@@ -13,9 +13,6 @@
 						    <header class="article-header">
 							
 							    <h1 class="page-title" itemprop="headline"><?php the_title(); ?></h1>
-                  <p class="byline vcard"><?php
-                    printf(__('Posted <time class="updated" datetime="%1$s" pubdate>%2$s</time> by <span class="author">%3$s</span>.', 'bonestheme'), get_the_time('Y-m-j'), get_the_time(__('F jS, Y', 'bonestheme')), bones_get_the_author_posts_link());
-                  ?></p>
 
 						
 						    </header> <!-- end article header -->
@@ -25,11 +22,11 @@
 							</section> <!-- end article section -->
 						
 						    <footer class="article-footer">
-                  <?php the_tags('<span class="tags">' . __('Tags:', 'bonestheme') . '</span> ', ', ', ''); ?>
+                  <?php //the_tags('<span class="tags">' . __('Tags:', 'bonestheme') . '</span> ', ', ', ''); ?>
 							
 						    </footer> <!-- end article footer -->
 						    
-						    <?php comments_template(); ?>
+						    <?php //comments_template(); ?>
 					
 					    </article> <!-- end article -->
 					
